66839d7cb97023486b5ee4d449188b6d758fcaeb
Margus1I <shahbozbegmatov77@gmail.com>
2024-06-23 13:21:46 +0300

    30.05.2024

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -4,13 +4,14 @@ import all_products from "../assets/all_products";
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
-    const contextValue = {all_products};
+    
+    const contextValue = { all_products };
 
-    return {
+    return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
-    }
+    )
 }
 
 export default ShopContextProvider;
